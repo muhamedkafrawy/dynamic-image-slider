35ee138efbb6865187f81bc25bb43c1276cf8a88
kafroo <kafration@gmail.com>
2020-05-04 14:58:53 +0200

    initial

---
+++ style/js/main.js
@@ -0,0 +1,34 @@
+// calling images, next button, and prev button 
+var slides = document.querySelector('.slider-items').children;
+var nextSlide = document.getElementById("next");
+var prevSlide = document.getElementById("prev");
+// to know number of images to slide throw
+var totalSlides = slides.length;
+var index = 0;
+// when clicking on the next and prev buttons
+nextSlide.onclick = function () {
+     next("next");
+}
+prevSlide.onclick = function () {
+     next("prev");
+}
+// to get next slides or previous slides 
+function next(direction) {
+     if (direction == "next") {
+          index++;
+          if (index == totalSlides) {
+               index = 0;
+          }
+     }
+     else {
+          if (index == 0) {
+               index = totalSlides - 1;
+          } else {
+               index--;
+          }
+     }
+     for (i = 0; i < totalSlides; i++) {
+          slides[i].classList.remove("active");
+     }
+     slides[index].classList.add("active");
+}
