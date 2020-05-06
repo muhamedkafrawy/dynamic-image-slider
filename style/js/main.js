7da5de0abf60d5f16ad598192617f9311c12b8f6
kafroo <kafration@gmail.com>
2020-05-06 14:13:54 +0200

    added another image slider

--- style/js/main.js
+++ style/js/main.js
@@ -1,34 +1,42 @@
 // calling images, next button, and prev button 
-var slides = document.querySelector('.slider-items').children;
-var nextSlide = document.getElementById("next");
-var prevSlide = document.getElementById("prev");
-// to know number of images to slide throw
-var totalSlides = slides.length;
-var index = 0;
-// when clicking on the next and prev buttons
-nextSlide.onclick = function () {
-     go("next");
-}
-prevSlide.onclick = function () {
-     go("prev");
-}
-// to get next slides or previous slides 
-function go(direction) {
-     if (direction == "next") {
-          index++;
-          if (index == totalSlides) {
-               index = 0;
+let sliders = document.querySelectorAll('.slider');
+function slide(slider) {
+     let nextSlide = slider.querySelector('.next');
+     let prevSlide = slider.querySelector('.prev');
+     let slides = slider.querySelector('.slider-items').children;
+     // to know number of images to slide throw
+     let totalSlides = slides.length;
+     let index = 0;
+     // when clicking on the next and prev buttons
+     nextSlide.onclick = function () {
+          go("next");
+     }
+     prevSlide.onclick = function () {
+          go("prev");
+     }
+     // to get next slides or previous slides 
+     function go(direction) {
+          if (direction == "next") {
+               index++;
+               if (index == totalSlides) {
+                    index = 0;
+               }
           }
-     }
-     else {
-          if (index == 0) {
-               index = totalSlides - 1;
-          } else {
-               index--;
+          else {
+               if (index == 0) {
+                    index = totalSlides - 1;
+               } else {
+                    index--;
+               }
           }
+          for (i = 0; i < totalSlides; i++) {
+               slides[i].classList.remove("active");
+          }
+          slides[index].classList.add("active");
      }
-     for (i = 0; i < totalSlides; i++) {
-          slides[i].classList.remove("active");
-     }
-     slides[index].classList.add("active");
 }
+for (let slider of sliders) {
+     slide(slider);
+}
+
+
